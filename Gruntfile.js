d3aee06239074e211a81d944b2d2c64ee56d8947
Brandon Li <brandon.li820@icloud.com>
2019-11-02 13:38:42 -0600

    run Running "generate-gitignore" task
    Successfully generated!
    Done.

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+// Copyright © 2019 Brandon Li. All rights reserved.
+
+/**
+ * Grunt configuration file.
+ *
+ * @author Brandon Li <brandon.li820@gmail.com>
+ */
+
+module.exports = require( '@brandonli8/grunt-config/Gruntfile.js' );
