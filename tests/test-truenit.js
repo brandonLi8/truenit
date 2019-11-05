981a9a929343aa1ab27fbbcb08cd98cd038631c8
Brandon Li <brandon.li820@icloud.com>
2019-11-04 19:58:22 -0700

    #11 reformat tests

--- tests/test-truenit.js
+++ tests/test-truenit.js
@@ -16,129 +16,122 @@
   'use strict';
 
   // modules
-  // const truenit = require( '../src/truenit' );
+  const truenit = require( '../src/truenit' );
   const utils = require( '../src/utils' );
 
-  // //----------------------------------------------------------------------------------------
-  // // Test 1: Register passing tests start.
-  // //----------------------------------------------------------------------------------------
+  //----------------------------------------------------------------------------------------
+  // Test 1: Register passing tests start.
+  //----------------------------------------------------------------------------------------
 
-  // line();
-  // printBold( 'Test 1...\n\n' );
+  utils.println( '|———————————————————————————————————————————————————————————————————————————|\n', 1 );
+  utils.print( '| Test 1...                                                                 |\n', 0, 1 );
+  utils.print( '|———————————————————————————————————————————————————————————————————————————|\n\n', 1 );
 
-  // truenit.registerTest( 'Module_1_should_pass', () => {
-  //   truenit.ok( 5 === 5, 'will not be called' );
-  // } );
+  truenit.registerTest( 'Module_1_should_pass', () => {
+    truenit.ok( 5 === 5, 'will not be called' );
+  } );
 
-  // truenit.registerTest( 'Module_2_should_pass_but_has_a_long_name_but_should_still_format_nicely', () => {
-  //   truenit.ok( typeof 'str' === 'string', 'will not be called' );
-  // } );
+  truenit.registerTest( 'Module_2_should_pass_but_has_a_long_name_but_should_still_format_nicely', () => {
+    truenit.ok( typeof 'str' === 'string', 'will not be called' );
+  } );
 
-  // truenit.registerTest( 'Module_3', () => {
-  //   truenit.notOk( false, 'will not be called' );
-  // } );
+  truenit.registerTest( 'Module_3', () => {
+    truenit.notOk( false, 'will not be called' );
+  } );
 
-  // truenit.start();
-  // truenit.clearTests(); // clear all registered tests for the next test
+  truenit.start();
+  truenit.clearTests(); // clear all registered tests for the next test
 
-  // printBold( 'Test 1 passed!\n' );
+  utils.print( '|———————————————————————————————————————————————————————————————————————————\n', 1, 32 );
+  utils.print( '| Test 1 passed!                                                            \n', 1, 32 );
+  utils.print( '|———————————————————————————————————————————————————————————————————————————\n', 1, 32 );
 
-  // //----------------------------------------------------------------------------------------
-  // // Test 2: don't register multiple tests that pass and start.
-  // //----------------------------------------------------------------------------------------
-  // line();
-  // printBold( 'Test 2...\n\n' );
+  //----------------------------------------------------------------------------------------
+  // Test 2: don't register multiple tests that pass and start.
+  //----------------------------------------------------------------------------------------
+  utils.println( '|———————————————————————————————————————————————————————————————————————————|\n', 1 );
+  utils.print( '| Test 2...                                                                 |\n', 0, 1 );
+  utils.print( '|———————————————————————————————————————————————————————————————————————————|\n\n', 1 );
 
-  // truenit.test( 'Module_1_should_pass', () => {
-  //   truenit.notOk( 6 === 5, 'will not be called' );
-  // } );
+  truenit.test( 'Module_1_should_pass', () => {
+    truenit.notOk( 6 === 5, 'will not be called' );
+  } );
 
-  // truenit.test( 'Module_2_should_pass_but_has_a_long_name_but_will_not_format_nicely_since_not_registered', () => {
-  //   truenit.ok( typeof 'str' === 'string', 'will not be called' );
-  // } );
+  truenit.test( 'Module_2_should_pass_but_has_a_long_name_but_will_not_format_nicely_since_not_registered', () => {
+    truenit.ok( typeof 'str' === 'string', 'will not be called' );
+  } );
 
-  // truenit.test( 'Module_3', () => {
-  //   truenit.ok( true, 'will not be called' );
-  // } );
+  truenit.test( 'Module_3', () => {
+    truenit.ok( true, 'will not be called' );
+  } );
 
-  // truenit.start(); // shouldn't log anything
+  truenit.start(); // shouldn't log anything
 
-  // printBold( '\nTest 2 passed!\n' );
+  utils.println( '|———————————————————————————————————————————————————————————————————————————\n', 1, 32 );
+  utils.print( '| Test 2 passed!                                                            \n', 1, 32 );
+  utils.print( '|———————————————————————————————————————————————————————————————————————————\n', 1, 32 );
 
-  // //----------------------------------------------------------------------------------------
-  // // Test 3: don't register multiple tests with some that fail and start.
-  // //----------------------------------------------------------------------------------------
-  // line();
-  // printBold( 'Test 3:\n\n',  );
+  //----------------------------------------------------------------------------------------
+  // Test 3: don't register multiple tests with some that fail and start.
+  //----------------------------------------------------------------------------------------
+  utils.println( '|———————————————————————————————————————————————————————————————————————————|\n', 1 );
+  utils.print( '| Test 3...                                                                 |\n', 0, 1 );
+  utils.print( '|———————————————————————————————————————————————————————————————————————————|\n\n', 1 );
+  truenit.test( 'Module_1_should_pass', () => {
+    truenit.ok( 5 === 5, 'will not be called' );
+  } );
 
-  // truenit.test( 'Module_1_should_pass', () => {
-  //   truenit.ok( 5 === 5, 'will not be called' );
-  // } );
-
-  // truenit.test( 'Module_will_fail', () => {
-  //   truenit.notOk( 5 === 6, 'will not be called' );
-  // } );
-
-
-  // truenit.throws( 'Module_will_throw_error', () => {
-  //   truenit.ok( 6 === 5, 'will not be called' );
-  // } );
-
-  // printBold( '\nTest 3 passed!\n' );
-
-
-  // //----------------------------------------------------------------------------------------
-  // // Test 4: register multiple tests with some that fail and start.
-  // //----------------------------------------------------------------------------------------
-  // line();
-  // printBold( 'Test 4:\n\n',  );
-
-  // truenit.registerTest( 'Module_1_should_pass', () => {
-  //   truenit.notOk( 7 === 5, 'will not be called' );
-  // } );
-
-  // truenit.registerThrowTest( 'Module_will_fail', () => {
-  //   truenit.ok( 5 === 6, 'will not be called' );
-  // } );
-
-  // const moduleNotTested = 'ModuleNotTested';
-  // const moduleNotTester = () => {
-  //   truenit.ok( 6 === 5, 'will not be called' );
-  // };
-
-  // truenit.registerTest( moduleNotTested, moduleNotTester );
-  // truenit.removeTest( moduleNotTested, moduleNotTester );
+  truenit.test( 'Module_will_fail', () => {
+    truenit.notOk( 5 === 6, 'will not be called' );
+  } );
 
 
-  // truenit.registerTest( 'Module_1_should_pass', () => {
-  //   truenit.ok( 5 === 5, 'will not be called' );
-  // } );
+  truenit.throws( 'Module_will_throw_error', () => {
+    truenit.ok( 6 === 5, 'will not be called' );
+  } );
 
-  // truenit.start();
-
-  // printBold( '\nTest 4 passed!\n' );
-
-
-  // utils.print( '\nAll Test Passed. Bye!\n', 32 );
+  utils.println( '|———————————————————————————————————————————————————————————————————————————\n', 1, 32 );
+  utils.print( '| Test 3 passed!                                                            \n', 1, 32 );
+  utils.print( '|———————————————————————————————————————————————————————————————————————————\n', 1, 32 );
 
 
-  // //----------------------------------------------------------------------------------------
-  // // Helpers
-  // //----------------------------------------------------------------------------------------
+  //----------------------------------------------------------------------------------------
+  // Test 4: register multiple tests with some that fail and start.
+  //----------------------------------------------------------------------------------------
+  utils.println( '|———————————————————————————————————————————————————————————————————————————\n' );
+  utils.print( '| Test 4...                                                                 \n', 0 );
+  utils.print( '|———————————————————————————————————————————————————————————————————————————\n\n' );
 
-  // /**
-  //  * Prints a line.
-  //  */
-  // function line() {
-  //   utils.println( '————————————————————————————————————————————————————————————————————————————\n\n', 2 );
-  // }
-  // /**
-  //  * Prints in bold and underlined.
-  //  */
-  // function printBold( text ) {
-  //   utils.print( '\x1b[1m\x1b[4m' + text, 1 );
-  // }
+  truenit.registerTest( 'Module_1_should_pass', () => {
+    truenit.notOk( 7 === 5, 'will not be called' );
+  } );
+
+  truenit.registerThrowTest( 'Module_will_fail', () => {
+    truenit.ok( 5 === 6, 'will not be called' );
+  } );
+
+  const moduleNotTested = 'ModuleNotTested';
+  const moduleNotTester = () => {
+    truenit.ok( 6 === 5, 'will not be called' );
+  };
+
+  truenit.registerTest( moduleNotTested, moduleNotTester );
+  truenit.removeTest( moduleNotTested, moduleNotTester );
 
 
-  utils.print( 'hello world', 2, 30, 47, 36 )
+  truenit.registerTest( 'Module_1_should_pass', () => {
+    truenit.ok( 5 === 5, 'will not be called' );
+  } );
+
+  truenit.start();
+
+  utils.print( '|———————————————————————————————————————————————————————————————————————————\n', 32 );
+  utils.print( '| Test 4 passed!                                                            \n', 32 );
+  utils.print( '|———————————————————————————————————————————————————————————————————————————\n', 32 );
+  
+
+  utils.println( '|———————————————————————————————————————————————————————————————————————————\n', 1, 32 );
+  utils.print( '| All Tests Passed! Bye!                                                    \n', 1, 32 );
+  utils.print( '|———————————————————————————————————————————————————————————————————————————\n', 1, 32 );
+
 } )();
